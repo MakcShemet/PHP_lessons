2ac1fbd08d8c8c76d049db22f56c5c882b46e267
DevMasterSh <dev_master_sh@mail.ru>
2024-02-12 20:03:57 +0300

    Solution homework 5

--- Homework_5/code/src/Controllers/UserController.php
+++ Homework_5/code/src/Controllers/UserController.php
@@ -4,33 +4,51 @@ namespace Geekbrains\Application1\Controllers;
 
 use Geekbrains\Application1\Render;
 use Geekbrains\Application1\Models\User;
+use Geekbrains\Application1\Models\Message;
 
-class UserController {
+class UserController
+{
 
-    public function actionAddUser() {
-        return "Тут добавляется юзер";
-    }
 
-    public function actionIndex() {
+    public function actionIndex()
+    {
         $users = User::getAllUsersFromStorage();
-        
+
         $render = new Render();
 
-        if(!$users){
+        if (!$users) {
             return $render->renderPage(
                 'user-empty.twig',
                 [
                     'title' => 'Список пользователей в хранилище',
                     'message' => "Список пуст или не найден"
-                ]);
-        }
-        else{
+                ]
+            );
+        } else {
             return $render->renderPage(
                 'user-index.twig',
                 [
                     'title' => 'Список пользователей в хранилище',
-                    'users' => $users
-                ]);
+                    'users' => $users,
+                ]
+            );
+        }
+    }
+
+    public function actionSave()
+    {
+        $address = "./storage/birthdays.txt";
+        $name = $_GET['name'];
+        $birthday = $_GET['birthday'];
+        $data = $name . ", " . $birthday . PHP_EOL;
+
+        $fileHandler = fopen($address, 'a');
+
+        if (fwrite($fileHandler, $data)) {
+            header("location: ../");
+            die();
+        } else {
+            return "Ошибка при попытке записи в файл. Запись не добавлена";
         }
     }
 }
